d6a65ea6fd9e8aac964ecd654d0ba382d3baf890
guojiaxin-cykf <2776250758@qq.com>
2020-05-29 16:16:51 +0800

    keep-alive性能优化

--- my-project/src/pages/home/Home.vue
+++ my-project/src/pages/home/Home.vue
@@ -15,6 +15,7 @@
   import HomeRecommend from './components/Recommend'
   import HomeWeekend from './components/Weekend'
   import axios from 'axios'
+  import { mapState } from 'vuex'
   export default {
     name:'Home',
     components:{
@@ -24,8 +25,12 @@
       HomeRecommend:HomeRecommend,
       HomeWeekend:HomeWeekend,
     },
+    computed:{
+      ...mapState(['city'])
+    },
     data:function(){
       return{
+        lastCity:'',
         swiperList: [],
         iconList:[],
         recommendList:[],
@@ -34,7 +39,7 @@
     },
     methods:{
       getHomeInfo(){
-        axios.get('/api/index.json')
+        axios.get('/api/index.json?city=' + this.city)
           .then(this.getHomeInfoSucc)
       },
       getHomeInfoSucc(res){
@@ -49,7 +54,14 @@
       }
     },
     mounted() {
+      this.lastCity = this.city
       this.getHomeInfo()
+    },
+    activated() {
+      if(this.lastCity !== this.city){
+        this.lastCity = this.city
+        this.getHomeInfo()
+      }
     }
   }
 </script>
